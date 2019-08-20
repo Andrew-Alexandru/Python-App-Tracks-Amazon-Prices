e4fd06e7664c704a5e36784f920353978ca7e77c
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2019-08-20 13:21:49 +0300

    Import requests and BeautifulSoup from bs4.Also search for an URL and your user agent

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.de/Sony-Digitalkamera-Touch-Display-Vollformatsensor-Kartenslots/dp/B07B4R8QGM/ref=sr_1_5?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=sony+a7&qid=1566296312&s=gateway&sr=8-5'
+
+headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+
+page = requests.get(URL, headers=headers)
